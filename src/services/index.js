eecab5a1bde0b838e6f0d8f76b7f0ae9cb1a924c
tusharrajput001 <tusharr0491@gmail.com>
2025-08-30 12:54:26 +0530

    services created

---
+++ src/services/index.js
@@ -0,0 +1,7 @@
+// Export all services
+export { default as apiService } from './api/apiService.js';
+export { default as authService } from './auth/authService.js';
+
+// You can add more services here as they are created
+// export { default as userService } from './user/userService.js';
+// export { default as fitnessService } from './fitness/fitnessService.js';
